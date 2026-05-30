5c0fe1838cd2c17472f8d702557b7c7fde6d4baf
kumongul <iplayens3@gmail.com>
2026-05-30 19:17:54 +0000

    leetcode: valid-parentheses

--- main.go
+++ main.go
@@ -103,3 +103,37 @@ func moveZeroes(nums []int) {
 		nums[i] = 0
 	}
 }
+
+// 6
+func validParentheses(s string) bool {
+	pairs := map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+
+	var stack []rune
+
+	for _, ch := range s {
+
+		// 1. если ОТКРЫВАЮЩАЯ → push
+		if ch == '(' || ch == '[' || ch == '{' {
+			stack = append(stack, ch)
+			continue
+		}
+
+		if ch == ')' || ch == ']' || ch == '}' {
+			if len(stack) == 0 {
+				return false
+			}
+			top := stack[len(stack)-1]
+			if pairs[top] == ch {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+			return false
+		}
+	}
+
+	return len(stack) == 0
+}
