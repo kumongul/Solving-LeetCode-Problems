4a6557e8f1dba6c0cad688459d74bab675caae70
kumongul <iplayens3@gmail.com>
2026-05-31 13:30:42 +0000

    leetcode: Best-Time to Buy and Sell-Stock

--- main.go
+++ main.go
@@ -137,3 +137,28 @@ func validParentheses(s string) bool {
 
 	return len(stack) == 0
 }
+
+// 7
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	minPrice := prices[0]
+	maxProfit := 0
+
+	for i := 1; i < len(prices); i++ {
+
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		}
+
+		profit := prices[i] - minPrice
+
+		if profit > maxProfit {
+			maxProfit = profit
+		}
+	}
+
+	return maxProfit
+}
